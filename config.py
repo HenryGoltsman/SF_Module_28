6c6666afedb3337f57edb1eab1a91f25b8b2d70b
Henry <goltsmangenrih@gmail.com>
2022-12-21 17:33:11 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,25 @@
+correct_name = 'Генри'
+correct_surname = 'Харт'
+correct_email = 'henrygolcman@gmail.com'
+correct_password = 'WSVHheDaNH92cSg'
+correct_code = '530814'
+
+incorrect_name = '123456'
+incorrect_surname = '123456'
+incorrect_email = '123456'
+incorrect_password = '123456'
+incorrect_code = '123456456'
+
+incorrect_name_2 = '!@#$%^&*'
+incorrect_surname_2 = '!@#$%^&*'
+incorrect_email_2 = '!@#$%^&*'
+incorrect_password_2 = '!@#$%^&*'
+
+incorrect_name_3 = 'йцкенqwerty'
+incorrect_surname_3 = 'йцукенqwerty'
+incorrect_email_3 = 'йцукенqwerty'
+incorrect_password_3 = 'йцукенqwerty'
+
+incorrect_password_4 = 'yahoo4Win'
+
+correct_phone = '+79165019704'
